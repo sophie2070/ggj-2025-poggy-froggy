68e171934c6929bb568ec603d2ef45035997bb77
Marlies Kuipers <113506352+Marlies-boop@users.noreply.github.com>
2025-01-25 20:45:49 +0100

    added bubble respawn

--- Assets/Scripts/StaminaBubble.cs
+++ Assets/Scripts/StaminaBubble.cs
@@ -4,12 +4,17 @@ using UnityEngine;
 
 public class StaminaBubble : MonoBehaviour
 {
-
+    public float respawnTime = 3;
+    public bool respawning = false;
     Movement PlayerMovement;
+    SpriteRenderer spriteRenderer;
+    CircleCollider2D circleCollider;
 
     private void Awake()
     {
         PlayerMovement = FindAnyObjectByType<Movement>();
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+        circleCollider = gameObject.GetComponent<CircleCollider2D>();
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -17,7 +22,24 @@ public class StaminaBubble : MonoBehaviour
         if (collision.tag == "Player")
         {
             PlayerMovement.CanAirJump = true;
-            Destroy(this.gameObject);
+            respawning = true;
+            spriteRenderer.enabled = false;
+            circleCollider.enabled = false;
+        }
+    }
+
+    private void Update()
+    {
+        if(respawning)
+        {
+            respawnTime -= Time.deltaTime;
+        }
+        if(respawnTime <= 0)
+        {
+            spriteRenderer.enabled = true;
+            circleCollider.enabled = true;
+            respawning = false;
+            respawnTime = 3;
         }
     }
 }
