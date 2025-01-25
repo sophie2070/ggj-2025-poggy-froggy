1e41a114b80d19f4f5bd6a6c4552fbec039d0e21
Marlies Kuipers <113506352+Marlies-boop@users.noreply.github.com>
2025-01-25 17:31:10 +0100

    updated prefabs and finish

--- Assets/Scripts/Finish.cs
+++ Assets/Scripts/Finish.cs
@@ -5,10 +5,13 @@ public class Finish : MonoBehaviour
     public GameObject finish;
     
     Timer timer;
+    Movement movement;
+    public Vector2 movementSpeed;
 
     public void Start()
     {
         timer = GameObject.Find("TimerCanvas").GetComponent<Timer>();
+        movement = GameObject.Find("Player").GetComponent<Movement>();
         timer.timeStopped = false;
     }
 
@@ -16,5 +19,12 @@ public class Finish : MonoBehaviour
     {
         finish = GameObject.FindWithTag("Finish");
         timer.timeStopped = true;
+        movement.rb.gravityScale = 0f;
+        //movement.rb.bodyType.Equals(1);
+        //movement.rb.bodyType.Equals("Kinematic");
+        movement.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Static;
+        //movementSpeed = Vector2.zero;
+        //movement.GetComponent<Rigidbody2D>().angularVelocity = 0f;
+        movement.enabled = false;
     }
 }
