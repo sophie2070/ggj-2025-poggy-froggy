aaaa332335d59ed68710610e9b33718a3cf840ba
Marlies Kuipers <113506352+Marlies-boop@users.noreply.github.com>
2025-01-25 09:25:19 +0100

    timer stop and reset

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -2,31 +2,66 @@ using System;
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
+using UnityEngine.InputSystem;
 
 public class Timer: MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI timerText;
+    [SerializeField] float startTime = 150;
     [SerializeField] float remainingTime;
-    void Main(string[] args)
+    private float stopTime;
+    private float timerTime;
+    public bool timeStopped = false;
+
+
+    private void Start()
     {
+        remainingTime = startTime;
     }
 
     void Update()
     {
-        if (remainingTime > 0)
+        if (!timeStopped)
         {
-            remainingTime -= Time.deltaTime;
-        }
-        else if (remainingTime <= 0)
-        {
-            remainingTime = 0;
-            Debug.Log("Game Over");
+            if (remainingTime > 0)
+            {
+                remainingTime -= Time.deltaTime;
+            }
+            else if (remainingTime <= 0)
+            {
+                remainingTime = 0;
+                Debug.Log("Game Over");
+            }
         }
 
         int minutes = Mathf.FloorToInt(remainingTime / 60);
         int seconds = Mathf.FloorToInt(remainingTime % 60);
 
         timerText.text = String.Format("{0:00}:{1:00}", minutes, seconds);
+
+        if(Input.GetMouseButtonDown(0))
+        {
+            timeStopped = true;
+            StopTimer();
+        }
+
+        if(Input.GetMouseButtonDown (1))
+        {
+            ResetTimer();
+        }
+    }
+
+    void StopTimer()
+    {
+        if (timeStopped)
+        {
+            stopTime = remainingTime;
+        }
+    }
+
+    void ResetTimer()
+    {
+        remainingTime = startTime;
     }
 }
 
